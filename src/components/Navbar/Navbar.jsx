6176b43d78e3ded7bf0c4cdcfe98f63236f9362a
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-09-14 17:58:44 +0500

    commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+import digital from '../../Assests/Img/digital.png'
+import search from '../../Assests/Img/search.png'
+import phone from '../../Assests/Img/phone.png'
+
+function Navbar() {
+  return (
+    <div className="navbar">
+       <div className="container">
+           <Link className="navbar-link" to="/">
+              <img src={digital} alt="" className="navbar-img" />
+           </Link>
+           <form className="navbar-form">
+              <input type="text" required placeholder="Search..." className="navbar-input" />
+              <button type="submit" className="navbar-button">
+                <img src={search} alt="" className="navbar-image" />
+              </button>
+           </form>
+          <div className="navbar-list">
+              <a href="+74952217843" className="navbar-phone">
+                <img src={phone} alt="" className="navbar-pic" /> +7 (495) 221-78-43 
+              </a>
+          </div>
+       </div>
+    </div>
+  )
+}
+
+export default Navbar;
