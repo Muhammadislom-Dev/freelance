5b767097292dbbb40f32c414ab74595ff00c942a
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-09-16 01:50:31 +0500

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import './App.css';
+import Carousel from './components/Carousel/Carousel';
 import First from './components/First/First';
+import { Header } from './components/Header/Header';
+import { Home } from './components/Home/Home';
 import Navbar from './components/Navbar/Navbar';
 import Second from './components/Second/Second';
 import Section from './components/Section/Section';
+import TextPage from './components/TextPage/TextPage';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Header />
+      <Home />
       <Section />
       <First />
       <Second />
+      <Carousel />
+      <TextPage />
     </div>
   );
 }
