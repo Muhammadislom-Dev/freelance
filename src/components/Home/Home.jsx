3c71565aa3b914c84829cfca2c2c2020a3302da9
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-09-21 18:45:48 +0500

    add

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,136 +1,136 @@
-import { Link } from 'react-router-dom';
-import './Home.css';
-
+import { Link } from "react-router-dom";
+import "./Home.css";
+import notebook from '../../Assests/Img/image-3.webp'
 
 export const Home = () => {
-	return (
-		<div>
-			<div
-				id='carouselExampleControls'
-				className='carousel slide'
-				data-bs-ride='carousel'>
-				<div className='carousel-inner'>
-					<div className='carousel-item active'>
-						<div className='slide-inner'>
-							<div className='slide-info'>
-								<h2
-									className={{
-										marginTop: '0',
-										fontSize: '26px',
-									}}>
-									Наклеейки / стикеры / стикерпаки
-								</h2>
-								<ul>
-									<li>Срочная печать контурных стикеров</li>
-									<li>Печать стикеров с золотом</li>
-									<li>Профессионалоная отделка</li>
-									<li>
-										Загрузите файлы онлайн 24/7 прямо на
-										сайте{' '}
-									</li>
-									<li>Доставка от 300р</li>
-								</ul>
-								<Link
-									className={{
-										backgroundColor: 'primary',
-										display: 'inline-block',
-										padding: '5px 15px',
-										fontSize: '20px',
-									}}
-									to='/'>
-									Заказать
-								</Link>
-							</div>
-						</div>
-					</div>
-					<div className='carousel-item'>
-						<div className='slide-inner'>
-							<div className='slide-info'>
-								<h2
-									className={{
-										marginTop: '0',
-										fontSize: '26px',
-									}}>
-									Наклеейки / стикеры / стикерпаки
-								</h2>
-								<ul>
-									<li>Срочная печать контурных стикеров</li>
-									<li>Печать стикеров с золотом</li>
-									<li>Профессионалоная отделка</li>
-									<li>
-										Загрузите файлы онлайн 24/7 прямо на
-										сайте{' '}
-									</li>
-									<li>Доставка от 300р</li>
-								</ul>
-								<Link
-									className={{
-										backgroundColor: 'primary',
-										display: 'inline-block',
-										padding: '5px 15px',
-										fontSize: '20px',
-									}}
-									to='/'>
-									Заказать
-								</Link>
-							</div>
-						</div>
-					</div>
-					<div className='carousel-item'>
-						<div className='slide-inner'>
-							<div className='slide-info'>
-								<h2
-									className={{
-										marginTop: '0',
-										fontSize: '26px',
-									}}>
-									Наклеейки / стикеры / стикерпаки
-								</h2>
-								<ul>
-									<li>Срочная печать контурных стикеров</li>
-									<li>Печать стикеров с золотом</li>
-									<li>Профессионалоная отделка</li>
-									<li>
-										Загрузите файлы онлайн 24/7 прямо на
-										сайте{' '}
-									</li>
-									<li>Доставка от 300р</li>
-								</ul>
-								<Link
-									className={{
-										backgroundColor: 'primary',
-										display: 'inline-block',
-										padding: '5px 15px',
-										fontSize: '20px',
-									}}
-									to='/'>
-									Заказать
-								</Link>
-							</div>
-						</div>
-					</div>
-				</div>
-				<button
-					className='carousel-control-prev'
-					type='button'
-					data-bs-target='#carouselExampleControls'
-					data-bs-slide='prev'>
-					<span
-						className='carousel-control-prev-icon'
-						aria-hidden='true'></span>
-					<span className='visually-hidden'>Previous</span>
-				</button>
-				<button
-					className='carousel-control-next'
-					type='button'
-					data-bs-target='#carouselExampleControls'
-					data-bs-slide='next'>
-					<span
-						className='carousel-control-next-icon'
-						aria-hidden='true'></span>
-					<span className='visually-hidden'>Next</span>
-				</button>
-			</div>
-		</div>
-	);
+  return (
+    <div className="home">
+      <div
+        id="carouselExampleControls"
+        className="carousel slide"
+        data-bs-ride="carousel"
+      >
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <div className="slide-inner">
+              <div className="slide-info">
+                <h2
+                  className="slide-names"
+				  style={{color:"#F3C136"}}
+                >
+                  Your next great adventure.
+                </h2>
+                <h2 className="slide-name">
+                  Copying and <br /> Printing Center
+                </h2>
+                <p className="slide-text">
+                  Discover your next great adventure.
+                </p>
+                <Link
+                  className={{
+                    backgroundColor: "#FF4328",
+                    display: "inline-block",
+                    padding: "10px 20px",
+                    fontSize: "20px",
+                  }}
+                  to="/"
+                >
+                  Learn More
+                </Link>
+              </div>
+			  <div className="slide-left">
+				<img src={notebook} alt="" className="slide-img" />
+			  </div>
+            </div>
+          </div>
+          <div className="carousel-item">
+            <div className="slide-inner">
+              <div className="slide-info">
+                <h2
+                   className="slide-names"
+				   style={{color:"#F3C136"}}
+                >
+                  Your next great adventure.
+                </h2>
+                <h2 className="slide-name">
+                  Copying and <br /> Printing Center
+                </h2>
+                <p className="slide-text">
+                  Discover your next great adventure.
+                </p>
+                <Link
+                  className={{
+                    backgroundColor: "#FF4328",
+                    display: "inline-block",
+                    padding: "10px 20px",
+                    fontSize: "20px",
+                  }}
+                  to="/"
+                >
+                  Learn More
+                </Link>
+              </div>
+			  <div className="slide-left">
+				<img src={notebook} alt="" className="slide-img" />
+			  </div>
+            </div>
+          </div>
+          <div className="carousel-item">
+            <div className="slide-inner">
+              <div className="slide-info">
+                <h2
+                   className="slide-names"  style={{color:"#F3C136"}}>
+                  Your next great adventure.
+                </h2>
+                <h2 className="slide-name">
+                  Copying and <br /> Printing Center
+                </h2>
+                <p className="slide-text">
+                  Discover your next great adventure.
+                </p>
+                <Link
+                  className={{
+                    backgroundColor: "#FF4328",
+                    display: "inline-block",
+                    padding: "10px 20px",
+                    fontSize: "20px",
+                  }}
+                  to="/"
+                >
+                  Learn More
+                </Link>
+              </div>
+			  <div className="slide-left">
+				<img src={notebook} alt="" className="slide-img" />
+			  </div>
+            </div>
+          </div>
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleControls"
+          data-bs-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleControls"
+          data-bs-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+    </div>
+  );
 };
