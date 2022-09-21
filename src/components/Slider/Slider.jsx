35965666c9aec2095dd43ebc2d0c027a75bce864
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-09-21 18:46:15 +0500

    add

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,9 +1,8 @@
-import { Splide, SplideSlide, SplideTrack } from '@splidejs/react-splide'
-import React from 'react'
-import '@splidejs/react-splide/css'; 
-import './Slider.css'
-import aa from '../../Assests/Img/A.png'
-
+import { Splide, SplideSlide, SplideTrack } from "@splidejs/react-splide";
+import React from "react";
+import "@splidejs/react-splide/css";
+import "./Slider.css";
+import aa from "../../Assests/Img/A.png";
 
 const splideOpts = {
   rewind: true,
@@ -13,174 +12,159 @@ const splideOpts = {
   perMove: 1,
   breakpoints: {
     1000: {
-      perPage: 2
+      perPage: 2,
     },
     700: {
-      perPage: 1
-    }
-  }
-}
+      perPage: 1,
+    },
+  },
+};
 
 export default function Slider() {
   return (
-    <div style={{padding:"50px 0"}} className='container'>
-      <Splide  hasTrack={false} options={splideOpts}>
-        <SplideTrack>
-        <SplideSlide>
-            <div className='slide__container'>
-              <div className='slide__header'>
-                <img
-                  src={aa}
-                  alt="A"
-                  className='slide__img'
-                />
-                <div className='slide__info'>
-                  <h3 className='slide__title'>assadasd</h3>
-                  <p className='slide__date'>
-                    <span>asdasdasdasd</span>
-                    <span>32.43.2343</span>
-                  </p>
+    <div className="slider">
+      <div style={{ padding: "50px 0" }} className="containers">
+        <Splide hasTrack={false} options={splideOpts}>
+          <SplideTrack>
+            <SplideSlide>
+              <div className="slide__container">
+                <div className="slide__header">
+                  <img src={aa} alt="A" className="slide__img" />
+                  <div className="slide__info">
+                    <h3 className="slide__title">Saul Ramirez</h3>
+                    <p className="slide__date">
+                      <span>asdasdasdasd</span>
+                      <span>32.43.2343</span>
+                    </p>
+                    <p className="slide__text">
+                     Lorem ipsum dolor sit amet contur adipiscing elit vam us lacinia odio vestibulum.
+                    </p>
+                  </div>
                 </div>
               </div>
-              <p className='slide__text'>adlahsdjhd ajsdh jsadhjasdh ajdhjasdh ajsdhajsdh jasdhjasdhas hdas</p>
-            </div>
-          </SplideSlide>
-          <SplideSlide>
-            <div className='slide__container'>
-              <div className='slide__header'>
-                <img
-                   src={aa}
-                  alt="A"
-                  className='slide__img'
-                />
-                <div className='slide__info'>
-                  <h3 className='slide__title'>assadasd</h3>
-                  <p className='slide__date'>
-                    <span>asdasdasdasd</span>
-                    <span>32.43.2343</span>
-                  </p>
+            </SplideSlide>
+            <SplideSlide>
+              <div className="slide__container">
+                <div className="slide__header">
+                  <img src={aa} alt="A" className="slide__img" />
+                  <div className="slide__info">
+                    <h3 className="slide__title">Saul Ramirez</h3>
+                    <p className="slide__date">
+                      <span>asdasdasdasd</span>
+                      <span>32.43.2343</span>
+                    </p>
+                    <p className="slide__text">
+                     Lorem ipsum dolor sit amet contur adipiscing elit vam us lacinia odio vestibulum.
+                    </p>
+                  </div>
                 </div>
               </div>
-              <p className='slide__text'>adlahsdjhd ajsdh jsadhjasdh ajdhjasdh ajsdhajsdh jasdhjasdhas hdas</p>
-            </div>
-          </SplideSlide>
-          <SplideSlide>
-            <div className='slide__container'>
-              <div className='slide__header'>
-                <img
-                   src={aa}
-                  alt="A"
-                  className='slide__img'
-                />
-                <div className='slide__info'>
-                  <h3 className='slide__title'>assadasd</h3>
-                  <p className='slide__date'>
-                    <span>asdasdasdasd</span>
-                    <span>32.43.2343</span>
-                  </p>
+            </SplideSlide>
+            <SplideSlide>
+              <div className="slide__container">
+                <div className="slide__header">
+                  <img src={aa} alt="A" className="slide__img" />
+                  <div className="slide__info">
+                    <h3 className="slide__title">Saul Ramirez</h3>
+                    <p className="slide__date">
+                      <span>asdasdasdasd</span>
+                      <span>32.43.2343</span>
+                    </p>
+                    <p className="slide__text">
+                     Lorem ipsum dolor sit amet contur adipiscing elit vam us lacinia odio vestibulum.
+                    </p>
+                  </div>
                 </div>
               </div>
-              <p className='slide__text'>adlahsdjhd ajsdh jsadhjasdh ajdhjasdh ajsdhajsdh jasdhjasdhas hdas</p>
-            </div>
-          </SplideSlide>
-          <SplideSlide>
-            <div className='slide__container'>
-              <div className='slide__header'>
-                <img
-                   src={aa}
-                  alt="A"
-                  className='slide__img'
-                />
-                <div className='slide__info'>
-                  <h3 className='slide__title'>assadasd</h3>
-                  <p className='slide__date'>
-                    <span>asdasdasdasd</span>
-                    <span>32.43.2343</span>
-                  </p>
+            </SplideSlide>
+            <SplideSlide>
+              <div className="slide__container">
+                <div className="slide__header">
+                  <img src={aa} alt="A" className="slide__img" />
+                  <div className="slide__info">
+                    <h3 className="slide__title">Saul Ramirez</h3>
+                    <p className="slide__date">
+                      <span>asdasdasdasd</span>
+                      <span>32.43.2343</span>
+                    </p>
+                    <p className="slide__text">
+                     Lorem ipsum dolor sit amet contur adipiscing elit vam us lacinia odio vestibulum.
+                    </p>
+                  </div>
                 </div>
               </div>
-              <p className='slide__text'>adlahsdjhd ajsdh jsadhjasdh ajdhjasdh ajsdhajsdh jasdhjasdhas hdas</p>
-            </div>
-          </SplideSlide>
-          <SplideSlide>
-            <div className='slide__container'>
-              <div className='slide__header'>
-                <img
-                   src={aa}
-                  alt="A"
-                  className='slide__img'
-                />
-                <div className='slide__info'>
-                  <h3 className='slide__title'>assadasd</h3>
-                  <p className='slide__date'>
-                    <span>asdasdasdasd</span>
-                    <span>32.43.2343</span>
-                  </p>
+            </SplideSlide>
+            <SplideSlide>
+              <div className="slide__container">
+                <div className="slide__header">
+                  <img src={aa} alt="A" className="slide__img" />
+                  <div className="slide__info">
+                    <h3 className="slide__title">Saul Ramirez</h3>
+                    <p className="slide__date">
+                      <span>asdasdasdasd</span>
+                      <span>32.43.2343</span>
+                    </p>
+                    <p className="slide__text">
+                     Lorem ipsum dolor sit amet contur adipiscing elit vam us lacinia odio vestibulum.
+                    </p>
+                  </div>
                 </div>
               </div>
-              <p className='slide__text'>adlahsdjhd ajsdh jsadhjasdh ajdhjasdh ajsdhajsdh jasdhjasdhas hdas</p>
-            </div>
-          </SplideSlide>
-          <SplideSlide>
-            <div className='slide__container'>
-              <div className='slide__header'>
-                <img
-                   src={aa}
-                  alt="A"
-                  className='slide__img'
-                />
-                <div className='slide__info'>
-                  <h3 className='slide__title'>assadasd</h3>
-                  <p className='slide__date'>
-                    <span>asdasdasdasd</span>
-                    <span>32.43.2343</span>
-                  </p>
+            </SplideSlide>
+            <SplideSlide>
+              <div className="slide__container">
+                <div className="slide__header">
+                  <img src={aa} alt="A" className="slide__img" />
+                  <div className="slide__info">
+                    <h3 className="slide__title">Saul Ramirez</h3>
+                    <p className="slide__date">
+                      <span>asdasdasdasd</span>
+                      <span>32.43.2343</span>
+                    </p>
+                    <p className="slide__text">
+                     Lorem ipsum dolor sit amet contur adipiscing elit vam us lacinia odio vestibulum.
+                    </p>
+                  </div>
                 </div>
               </div>
-              <p className='slide__text'>adlahsdjhd ajsdh jsadhjasdh ajdhjasdh ajsdhajsdh jasdhjasdhas hdas</p>
-            </div>
-          </SplideSlide>
-          <SplideSlide>
-            <div className='slide__container'>
-              <div className='slide__header'>
-                <img
-                   src={aa}
-                  alt="A"
-                  className='slide__img'
-                />
-                <div className='slide__info'>
-                  <h3 className='slide__title'>assadasd</h3>
-                  <p className='slide__date'>
-                    <span>asdasdasdasd</span>
-                    <span>32.43.2343</span>
-                  </p>
+            </SplideSlide>
+            <SplideSlide>
+              <div className="slide__container">
+                <div className="slide__header">
+                  <img src={aa} alt="A" className="slide__img" />
+                  <div className="slide__info">
+                    <h3 className="slide__title">Saul Ramirez</h3>
+                    <p className="slide__date">
+                      <span>asdasdasdasd</span>
+                      <span>32.43.2343</span>
+                    </p>
+                    <p className="slide__text">
+                     Lorem ipsum dolor sit amet contur adipiscing elit vam us lacinia odio vestibulum.
+                    </p>
+                  </div>
                 </div>
               </div>
-              <p className='slide__text'>adlahsdjhd ajsdh jsadhjasdh ajdhjasdh ajsdhajsdh jasdhjasdhas hdas</p>
-            </div>
-          </SplideSlide>
-          <SplideSlide>
-            <div className='slide__container'>
-              <div className='slide__header'>
-                <img
-                   src={aa}
-                  alt="A"
-                  className='slide__img'
-                />
-                <div className='slide__info'>
-                  <h3 className='slide__title'>assadasd</h3>
-                  <p className='slide__date'>
-                    <span>asdasdasdasd</span>
-                    <span>32.43.2343</span>
-                  </p>
+            </SplideSlide>
+            <SplideSlide>
+              <div className="slide__container">
+                <div className="slide__header">
+                  <img src={aa} alt="A" className="slide__img" />
+                  <div className="slide__info">
+                    <h3 className="slide__title">Saul Ramirez</h3>
+                    <p className="slide__date">
+                      <span>asdasdasdasd</span>
+                      <span>32.43.2343</span>
+                    </p>
+                    <p className="slide__text">
+                     Lorem ipsum dolor sit amet contur adipiscing elit vam us lacinia odio vestibulum.
+                    </p>
+                  </div>
                 </div>
               </div>
-              <p className='slide__text'>adlahsdjhd ajsdh jsadhjasdh ajdhjasdh ajsdhajsdh jasdhjasdhas hdas</p>
-            </div>
-          </SplideSlide>
-          
-        </SplideTrack>
-      </Splide>
+            </SplideSlide>
+          </SplideTrack>
+        </Splide>
+      </div>
     </div>
-  )
+  );
 }
