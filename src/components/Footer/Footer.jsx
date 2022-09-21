1f890e7d02ef0a549a46f4d7bfbd7fa33e324788
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-09-21 18:45:37 +0500

    add

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,111 +7,76 @@ function Footer() {
   return (
     <div className="footer">
       <div className="container">
-        <div className="footer-page">
-          <ul className="footer-list">
-            <li className="footer-item">
-              <h2 className="footer-name">Услуги онлайн-типографии</h2>
-            </li>
-            <li className="footer-item">
-              <a href="#" className="footer-link">
-                Цифровая оперативная полиграфия
-              </a>
-            </li>
-            <li className="footer-item">
-              <a href="#" className="footer-link">
-                Изготовление визиток срочно!
-              </a>
-            </li>
-            <li className="footer-item">
-              <a href="#" className="footer-link">
-                Изготовление наклеек
-              </a>
-            </li>
-            <li className="footer-item">
-              <a href="#" className="footer-link">
-                Распечатка цветных и ч.б. документов
-              </a>
-            </li>
-            <li className="footer-item">
-              <a href="#" className="footer-link">
-                Интерьерная широкоформатная печать
-              </a>
-            </li>
-            <li className="footer-item">
-              <a href="#" className="footer-link">
-                Специальные цены на круглые наклейки
-              </a>
-            </li>
-          </ul>
-          <ul className="footer-list">
-            <li className="footer-item">
-              <h2 className="footer-name">Услуги онлайн-типографии</h2>
-            </li>
-            <li className="footer-item">
-              <a href="#" className="footer-link">
-                Цифровая оперативная полиграфия
-              </a>
-            </li>
-            <li className="footer-item">
-              <a href="#" className="footer-link">
-                Изготовление визиток срочно!
-              </a>
-            </li>
-            <li className="footer-item">
-              <a href="#" className="footer-link">
-                Изготовление наклеек
-              </a>
-            </li>
-            <li className="footer-item">
-              <a href="#" className="footer-link">
-                Распечатка цветных и ч.б. документов
-              </a>
-            </li>
-            <li className="footer-item">
-              <a href="#" className="footer-link">
-                Интерьерная широкоформатная печать
-              </a>
-            </li>
-            <li className="footer-item">
-              <a href="#" className="footer-link">
-                Специальные цены на круглые наклейки
-              </a>
-            </li>
-          </ul>
-          <ul className="footer-list">
-            <li className="footer-item">
-              <h2 className="footer-name">Контакты онлайн-типографии №1</h2>
-            </li>
-            <li className="footer-item">
-              <a href="#" className="footer-link">
-                <img src={location} alt="" className="footer-logo" />
-                г. Москва, Тверская улица, 27, строение 1, подъезд 2
-              </a>
-            </li>
-            <li className="footer-item">
-              <a href="#" className="footer-link">
-                mail@digital-printing.ru
-              </a>
-            </li>
-            <li className="footer-item">
-              <a href="#" className="footer-link ">
-                <img src={location} alt="" className="footer-logo" />
-                г. Москва, ММДЦ "Москва-Сити", Пресненская наб., д. 12, "Башня
-              </a>
-            </li>
-            <li className="footer-item">
-              <a href="#" className="footer-link">
-                <img src={phones} alt="" className="footer-logo" />
-                +7 (495) 356-45-96
-              </a>
-            </li>
-            <li className="footer-item">
-              <a href="#" className="footer-link">
-                <img src={phones} alt="" className="footer-logo" />
-                +7 (495) 221-78-43
-              </a>
-            </li>
-          </ul>
+        <h2 className="footer-subname">Don’t Miss Out</h2>
+        <form action="" className="footer-form">
+          <input
+            type="email"
+            required
+            placeholder="Your Email"
+            className="footer-input"
+          />
+          <button type="submit" className="footer-btn">
+            Subscribe
+          </button>
+        </form>
+
+        <div className="footer-box">
+          <div className="footer-title">
+            <h4 className="footer-name">About Printify</h4>
+            <p className="footer-text">
+              Printify is an online design,copying and printing company that is
+              passionate about great design and the difference it can make to
+              our customers and the world. Colors launched back in 2006 with the
+              aim to disrupt the trillion-dollar global print industry.
+            </p>
+          </div>
+          <div className="footer-title">
+            <h4 className="footer-name">Services</h4>
+            <ul className="footer-list">
+              <li className="footer-item">
+                <a href="#" className="footer-link">Business Card</a>
+              </li>
+              <li className="footer-item">
+                <a href="#" className="footer-link">Business Card</a>
+              </li>
+              <li className="footer-item">
+                <a href="#" className="footer-link">Business Card</a>
+              </li>
+              <li className="footer-item">
+                <a href="#" className="footer-link">Business Card</a>
+              </li>
+              <li className="footer-item">
+                <a href="#" className="footer-link">Business Card</a>
+              </li>
+            </ul>
+          </div>
+          <div className="footer-title">
+            <h4 className="footer-name">Services</h4>
+            <ul className="footer-list">
+              <li className="footer-item">
+                <a href="#" className="footer-link">Business Card</a>
+              </li>
+              <li className="footer-item">
+                <a href="#" className="footer-link">Business Card</a>
+              </li>
+              <li className="footer-item">
+                <a href="#" className="footer-link">Business Card</a>
+              </li>
+              <li className="footer-item">
+                <a href="#" className="footer-link">Business Card</a>
+              </li>
+              <li className="footer-item">
+                <a href="#" className="footer-link">Business Card</a>
+              </li>
+            </ul>
+          </div>
+          <div className="footer-title">
+            <h4 className="footer-name">About Printify</h4>
+            <p className="footer-texts">000 8888 999</p>
+            <p className="footer-texts">needhelp@printify.com 88 New</p>
+            <p className="footer-texts">Street, Washington DC United</p>
+            <p className="footer-texts">States, America</p>
+          </div>
         </div>
       </div>
     </div>
