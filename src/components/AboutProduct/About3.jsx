723f0aee44e8acb11d663e46b2bfe65914b976da
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-09-23 15:28:46 +0500

    add

--- src/components/AboutProduct/About3.jsx
+++ src/components/AboutProduct/About3.jsx
@@ -1,23 +1,33 @@
 import React from "react";
-import P3 from "../../Assests/Img/p3.jpg";
+import blog from "../../Assests/Img/blog2.jpg";
 import "./About.css";
 
 const About3 = () => {
   return (
     <div className="aboutp">
       <div className="container">
-        <h1 className="about-name">Открытие раздела интерьерной печати</h1>
+        <h1 className="about__name">Lorem, ipsum dolor.</h1>
         <div className="large-container">
           <div className="top">
             <div className="img-container">
-              <img  className="about-img" src={P3} alt="" />
+              <img className="about-img" src={blog} alt="" />
             </div>
             <p className="about-text">
-              Мы рады сообщить вам о запуске раздела высококачественной
-              Интерьерной печати больших и малых форматов. К вашим услугам самые
-              разнообразные материалы от обычной бумаги, до плотных постеров с
-              накаткой на пенокартон форматом от А3 (420х297мм) до А0+ шириной
-              более метра (1120мм) и любой длиной
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Perspiciatis quae, maiores earum fuga enim labore doloremque
+              quaerat corrupti nostrum laudantium dolores facere ex
+              exercitationem! Accusamus eos quod tenetur aut harum. Lorem ipsum,
+              dolor sit amet consectetur adipisicing elit. Ratione neque rem
+              corrupti unde quae eius consectetur sequi sint quia nihil
+              laudantium molestias quisquam, illum officiis voluptatibus,
+              tenetur molestiae quibusdam veniam. Lorem ipsum dolor sit, amet
+              consectetur adipisicing elit. Repudiandae nisi, pariatur nesciunt,
+              maiores reiciendis dolores facilis nostrum autem deleniti
+              obcaecati, earum totam at cum? Fugiat rerum nesciunt amet odit
+              dolores. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Doloribus qui, voluptates nostrum porro aspernatur non veritatis
+              cumque voluptatem blanditiis nihil sint eaque iure iste
+              reprehenderit omnis, modi nesciunt eos sequi?
             </p>
           </div>
         </div>
