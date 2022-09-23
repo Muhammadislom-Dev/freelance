585ce7dec7ea08f09cb06541d9b07229a03b5ce3
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-09-23 15:28:02 +0500

    add

---
+++ src/data.js
@@ -0,0 +1,36 @@
+import elektr from "./Assests/Img/blog.jpg";
+
+const data = [
+    {
+        id: 1,
+        title:"Электроника",
+        img: elektr
+    },
+    {
+        id: 2,
+        title:"Электроника",
+        img: elektr
+    },
+    {
+        id: 3,
+        title:"Промо-сувениры",
+        img: elektr
+    },
+    {
+        id: 4,
+        title:"Электроника",
+        img: elektr
+    },
+    {
+        id: 5,
+        title:"Промо-сувениры",
+        img: elektr
+    },
+    {
+        id: 6,
+        title:"Промо-сувениры",
+        img: elektr
+    },
+]
+
+export default data
